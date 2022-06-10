326d980c8c7d7ef205006c96c7dad77a5e1cdac7
VIJAY CHHETRI <vijay.chhetri@xpo.com>
2022-06-11 00:24:59 +0530

    Adding structure to our component templates

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [{name: 'Sample Recipe', description:'This is a sample',imagePath:'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'},
+  {name: 'Sample Recipe', description:'This is a sample',imagePath:'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'}];
   constructor() { }
 
   ngOnInit(): void {
