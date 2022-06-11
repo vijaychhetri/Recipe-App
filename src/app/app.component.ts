437f163caf7d59a43b9ed949f4e07cd3e3542e8f
VIJAY CHHETRI <vijay.chhetri@xpo.com>
2022-06-11 14:47:16 +0530

    adding navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  selectedFeature = 'recipe';
   title = 'my-angular-app';
+  navigate(feature: string)
+  {
+    this.selectedFeature = feature;
+  }
 }
